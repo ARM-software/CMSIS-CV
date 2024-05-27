ab9c6f0d65991a6e5fbd24ae56b41c56f1be8d79
Xavier Bourguet <xavier.bourguet@arm.com>
2024-05-27 14:20:57 +0200

    commit before merging with update

--- Testing/board/debug.py
+++ Testing/board/debug.py
@@ -1,7 +1,32 @@
-from scripts.export import *
+#from scripts.export import *
+#
+#with open("results/output_0.dat","rb") as f:
+#         r = read_tensors(f)
+#
+#for i in r:
+#    print(i.tensor)
 
-with open("results/output_0.dat","rb") as f:
-         r = read_tensors(f)
 
-for i in r:
-    print(i.tensor)
+import cv2 as cv
+import numpy as np 
+from PIL import ImageOps
+import PIL
+import PIL.Image
+import io 
+import scripts.export
+from scripts.export import AlgoImage,serialize_tensors,read_tensors
+ 
+ 
+ 
+#with open("results/output_0.dat","rb") as f:
+#    r = read_tensors(f)
+ 
+#print(r[0].dim)
+ 
+ref = AlgoImage.open("references/dev/test_0_img_0.tiff").tensor
+out = AlgoImage.open("results/img/AC6/VHT-Corstone-300/dev/test_0_img_0.tiff").tensor
+ 
+print(ref-out)
+ 
+print(out[0,0])
+print(ref[0,0])
