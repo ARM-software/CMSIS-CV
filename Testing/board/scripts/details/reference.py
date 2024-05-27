299398e193d37c89e0c6f74e6cfd18675b738f0e
Xavier Bourguet <xavier.bourguet@arm.com>
2024-05-27 14:20:24 +0200

    update test-dev for new gaussian

--- Testing/board/scripts/details/reference.py
+++ Testing/board/scripts/details/reference.py
@@ -180,13 +180,13 @@ class GaussianFilter:
             # If we get the blur as it is, it will be recorded as an .npy file
             # It would be simpler with a gray8 as tiff image 
             # So we need to convert back to Pillow
-            #pil = PIL.Image.fromarray(blur)
-            #filtered.append(AlgoImage(pil))
+            pil = PIL.Image.fromarray(blur)
+            filtered.append(AlgoImage(pil))
             #
             # Our gaussian return a q15 so we can't use a Pillow picture.
             # We convert the result and write is as .npy
-            blur= blur.astype(np.int16)*127
-            filtered.append(AlgoImage(blur))
+            #blur= blur.astype(np.int16)
+            #filtered.append(AlgoImage(blur))
 
         # Record the filtered images
         for image_id,img in enumerate(filtered):
