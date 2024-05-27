29562d8bd940fa316f87aa01811674895790bca5
Xavier Bourguet <xavier.bourguet@arm.com>
2024-05-27 14:20:57 +0200

    commit before merging with update

--- Testing/board/cinput.h
+++ Testing/board/cinput.h
@@ -11,6 +11,7 @@ extern "C"
 #if defined(CINPUT)
     
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define NB_CINPUT 65768
 =======
 <<<<<<< HEAD
@@ -19,6 +20,9 @@ extern "C"
 #define NB_CINPUT 4216
 >>>>>>> ab9c6f0 (commit before merging with update)
 >>>>>>> aab5d3b (commit before merging with update)
+=======
+#define NB_CINPUT 70203
+>>>>>>> d895f96 (commit before merging with update)
 extern const uint8_t cinput[NB_CINPUT];
 
 #endif
