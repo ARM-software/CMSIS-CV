44e5d3ac0b58b1a5a4cc58b74c2d974d41d8586c
Xavier Bourguet <xavier.bourguet@arm.com>
2024-05-27 14:20:57 +0200

    commit before merging with update

--- Testing/board/test_config.h
+++ Testing/board/test_config.h
@@ -1,7 +1,11 @@
 #ifndef TEST_CONFIG_H
 #define TEST_CONFIG_H
 
+<<<<<<< HEAD
 #define TESTGROUP1
+=======
+#define TESTDEV
+>>>>>>> ab9c6f0 (commit before merging with update)
 
 #endif
 
--- Testing/browser/stream/tests/canny_edge/cv_canny_edge_ocv_scheduler.cpp
+++ Testing/browser/stream/tests/canny_edge/cv_canny_edge_ocv_scheduler.cpp
@@ -227,12 +227,15 @@ typedef struct {
     WebDisplay<int8_t,307200> *display1;
     WebDisplay<int8_t,307200> *display2;
 <<<<<<< HEAD
+<<<<<<< HEAD
     Duplicate<int8_t,153600,int8_t,153600> *dup0;
     Gray16ToGray8<int8_t,153600,int8_t,76800> *g16_to_g8;
     GaussianFilter<int8_t,76800,int8_t,153600> *gaussian;
     RGBA32ToGray8<int8_t,307200,int8_t,76800> *to_gray8;
     Gray16ToRGBA32<int8_t,153600,int8_t,307200> *to_rgba;
 =======
+=======
+>>>>>>> ab9c6f0 (commit before merging with update)
     Duplicate<int8_t,76800,int8_t,76800> *dup0;
     GaussianFilter<int8_t,76800,int8_t,76800> *gaussian;
     RGBAToGray8<int8_t,307200,int8_t,76800> *to_gray8;
@@ -319,10 +322,14 @@ init_cb_state();
         return(CG_MEMORY_ALLOCATION_FAILURE);
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
     nodes.cv_to_rgba = new Gray8ToRGBA32<int8_t,76800,int8_t,307200>(*(fifos.fifo5),*(fifos.fifo6));
 =======
     nodes.cv_to_rgba = new Gray8ToRGBA<int8_t,76800,int8_t,307200>(*(fifos.fifo4),*(fifos.fifo5));
 >>>>>>> c84958d (commit before merging with update)
+=======
+    nodes.cv_to_rgba = new Gray8ToRGBA<int8_t,76800,int8_t,307200>(*(fifos.fifo4),*(fifos.fifo5));
+>>>>>>> ab9c6f0 (commit before merging with update)
     if (nodes.cv_to_rgba==NULL)
     {
         return(CG_MEMORY_ALLOCATION_FAILURE);
