8825936a14aedd3e91b6890701e7115cce64cb2f
Xavier Bourguet <xavier.bourguet@arm.com>
2024-05-31 11:23:54 +0200

    restore create.bat

--- Testing/browser/build/create.bat
+++ Testing/browser/build/create.bat
@@ -1,4 +1,3 @@
-emcmake cmake -DCMSISDSP="C:\CMSIS-DSP" ^
-  -DOpenCV_DIR="C:\Users\xavbou01\CMSIS-CV\CMSIS-CV\Testing\browser\opencv\lib\cmake\opencv4" ^
+emcmake cmake -DCMSISDSP="...CMSIS-DSP" ^
+  -DOpenCV_DIR=".../install/lib/cmake/opencv4" ^
   -G "Unix Makefiles" ..
-
