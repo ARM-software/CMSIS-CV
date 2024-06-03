a3348d0074631ad42fa19b5ce010defefa02a514
Xavier Bourguet <xavier.bourguet@arm.com>
2024-06-03 09:15:48 +0200

    comment and test done

--- Testing/browser/web/canny_edge/uiconfig_ocv.js
+++ Testing/browser/web/canny_edge/uiconfig_ocv.js
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 var uiconfig={'param1': {'name': 'CMSIS-CV Canny Edge', 'values': [{'value': 78, 'name': 'low', 'min': 0, 'max': 255, 'step': 1}, {'value': 33, 'name': 'high', 'min': 0, 'max': 255, 'step': 1}]}, 'param2': {'name': 'OpenCV Canny Edge', 'values': [{'value': 108, 'name': 'low', 'min': 0, 'max': 200, 'step': 1}, {'value': 95, 'name': 'high', 'min': 0, 'max': 200, 'step': 1}]}, 'sched_init': 'canny_edge_ocv_init_cv', 'sched_run': 'canny_edge_ocv_stream', 'width': 320, 'height': 240};
 =======
 var uiconfig={'param1': {'name': 'CMSIS-CV Canny Edge', 'values': [{'value': 108, 'name': 'low', 'min': 0, 'max': 255, 'step': 1}, {'value': 95, 'name': 'high', 'min': 0, 'max': 255, 'step': 1}]}, 'param2': {'name': 'OpenCV Canny Edge', 'values': [{'value': 108, 'name': 'low', 'min': 0, 'max': 200, 'step': 1}, {'value': 95, 'name': 'high', 'min': 0, 'max': 200, 'step': 1}]}, 'sched_init': 'canny_edge_ocv_init_cv', 'sched_run': 'canny_edge_ocv_stream', 'width': 320, 'height': 240};
@@ -15,3 +16,6 @@ var uiconfig={'param1': {'name': 'CMSIS-CV Canny Edge', 'values': [{'value': 78,
 =======
 var uiconfig={'param1': {'name': 'CMSIS-CV Canny Edge', 'values': [{'value': 78, 'name': 'low', 'min': 0, 'max': 255, 'step': 1}, {'value': 33, 'name': 'high', 'min': 0, 'max': 255, 'step': 1}]}, 'param2': {'name': 'OpenCV Canny Edge', 'values': [{'value': 108, 'name': 'low', 'min': 0, 'max': 200, 'step': 1}, {'value': 73, 'name': 'high', 'min': 0, 'max': 200, 'step': 1}]}, 'sched_init': 'canny_edge_ocv_init_cv', 'sched_run': 'canny_edge_ocv_stream', 'width': 320, 'height': 240};
 >>>>>>> 62e9e18 (changed output datatype for sobel)
+=======
+var uiconfig={'param1': {'name': 'CMSIS-CV Canny Edge', 'values': [{'value': 78, 'name': 'low', 'min': 0, 'max': 255, 'step': 1}, {'value': 33, 'name': 'high', 'min': 0, 'max': 255, 'step': 1}]}, 'param2': {'name': 'OpenCV Canny Edge', 'values': [{'value': 108, 'name': 'low', 'min': 0, 'max': 200, 'step': 1}, {'value': 95, 'name': 'high', 'min': 0, 'max': 200, 'step': 1}]}, 'sched_init': 'canny_edge_ocv_init_cv', 'sched_run': 'canny_edge_ocv_stream', 'width': 320, 'height': 240};
+>>>>>>> cfe2057 (comment and test done)
