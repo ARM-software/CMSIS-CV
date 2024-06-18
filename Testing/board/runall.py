ca2d16bda47c04c8469b48c4c0a63dbfb35fc0b6
Christophe Favergeon <48906714+christophe0606@users.noreply.github.com>
2024-06-18 14:01:04 +0200

    Github action to run tests on commit and PR (#5)
    Add github action for running tests on each PR and commit to main

--- Testing/board/runall.py
+++ Testing/board/runall.py
@@ -5,6 +5,8 @@ import subprocess
 import os 
 import glob
 import sys 
+from os import environ
+
 
 # Description of all configurations compiler / cores are defined
 # in this file
@@ -26,6 +28,11 @@ from pathlib import Path
 Path("references").mkdir(parents=True, exist_ok=True)
 Path("inputs").mkdir(parents=True, exist_ok=True)
 Path("results/img").mkdir(parents=True, exist_ok=True)
+
+GHACTION = False 
+
+if "AVH_FVP_PLUGINS" in os.environ:
+    GHACTION = True
 
 parser = argparse.ArgumentParser(description='Parse test description')
 parser.add_argument('-avh', nargs='?',type = str, default="C:/Keil_v5/ARM/VHT", help="AVH folder")
@@ -81,6 +88,7 @@ def getError(l,s):
 
 def getMessage(l,s):
     return(f"\n[bold cyan]{s}[/bold cyan]")
+
 
 class Result:
     def __init__(self,msg,error=False):
@@ -140,7 +148,19 @@ configFiles={
 # Windows executable
 # (At some point this script will also support
 # unix)
-avhExe={
+avhUnixExe={
+    "CS310":"FVP_Corstone_SSE-310_Ethos-U65",
+    "CS300":"FVP_Corstone_SSE-300_Ethos-U55",
+    "M55":"FVP_MPS2_Cortex-M55",
+    "M33_DSP_FP":"FVP_MPS2_Cortex-M33",
+    "M7DP":"FVP_MPS2_Cortex-M7",
+    "M4FP":"FVP_MPS2_Cortex-M4",
+    "M3":"FVP_MPS2_Cortex-M3",
+    "M23":"FVP_MPS2_Cortex-M23",
+    "M0plus":"FVP_MPS2_Cortex-M0plus",
+}
+
+avhWindowsExe={
     "CS310":"VHT_Corstone_SSE-310.exe",
     "CS300":"VHT_Corstone_SSE-300_Ethos-U55.exe",
     "M55":"VHT_MPS2_Cortex-M55.exe",
@@ -164,7 +184,18 @@ def runAVH(live,build,core):
     if os.path.exists(elf):
         app = elf
     config = os.path.join("fvp_configs",configFiles[core])
-    avh = os.path.join(AVHROOT,avhExe[core])
+
+    if AVHROOT:
+       avhAttempt = os.path.join(AVHROOT,avhWindowsExe[core])
+       if os.path.exists(avhAttempt):
+          avh = avhAttempt
+   
+       avhAttempt = os.path.join(AVHROOT,avhUnixExe[core])
+       if os.path.exists(avhAttempt):
+          avh = avhAttempt
+    else:
+       avh = avhUnixExe[core]
+
     res=run(avh,"-f",config,app,live=live)
     return(res)
    
@@ -245,6 +276,11 @@ if not args.group is None:
 
 # Run the tests and log the result
 # in a summary.html file
+# 
+
+def live_display(live,latest):
+    if not GHACTION:
+        live.update(renderable=gen_table(latest))
 
 MAX_ROWS=4
 latest=[]
@@ -307,25 +343,25 @@ with Live(gen_table([]), refresh_per_second=4) as live:
                                 #live.console.print("Incremental build")
                                 msg += " Incremental build"
                                 latest[-1][-1]=msg
-                                live.update(renderable=gen_table(latest))
+                                live_display(live,latest)
                                 res=run("cbuild","-O" ,"cprj",'cmsiscv.csolution.yml',"--toolchain" ,c,"-c",buildFile,live=live)
                              else:
                                 #live.console.print("Rebuild all (and RTE update)")
                                 msg += " Rebuild all (and RTE update)"
                                 latest[-1][-1]=msg
-                                live.update(renderable=gen_table(latest))
+                                live_display(live,latest)
                                 res=run("cbuild","-O" ,"cprj",'cmsiscv.csolution.yml',"--update-rte","-r","--toolchain" ,c,"-c",buildFile,live=live)
                           else:
                              #live.console.print("Incremental build")
                              msg += " Incremental build"
                              latest[-1][-1]=msg
-                             live.update(renderable=gen_table(latest))
+                             live_display(live,latest)
                              res=run("cbuild","-O" ,"cprj",'cmsiscv.csolution.yml',"--toolchain" ,c,"-c",buildFile,live=live)
                        
                        
                           if res.error:
                               latest[-1][-1]="[red]Error cbuild"
-                              live.update(renderable=gen_table(latest))
+                              live_display(live,latest)
                               #printError(live,"Error cbuild")
                               print(f'<p><font color="red">Error building {testSuite["name"]}</font></p><PRE>',file=f)
                               print(res.msg,file=f)
@@ -333,15 +369,15 @@ with Live(gen_table([]), refresh_per_second=4) as live:
                               continue
                        if not args.norun and (not core is None):
                            latest[-1][-1]="Run AVH"
-                           live.update(renderable=gen_table(latest))
+                           live_display(live,latest)
                            #printSubTitle(live,"Run AVH")
                            clean_old_results()
                            res=runAVH(live,build,core)
                            if res.error:
                                latest[-1][-1]="[red]Error running AVH"
-                               live.update(renderable=gen_table(latest))
+                               live_display(live,latest)
                                #printError(live,"Error running AVH")
-                               print(f'<p><font color="red">Error running {testSuite["name"]} with {avhExe[core]}</font></p><PRE>',file=f)
+                               print(f'<p><font color="red">Error running {testSuite["name"]} with {avhUnixExe[core]}</font></p><PRE>',file=f)
                                print(res.msg,file=f)
                                print("</PRE>",file=f)
                                continue
@@ -350,12 +386,12 @@ with Live(gen_table([]), refresh_per_second=4) as live:
                                if had_error:
                                    ERROR_OCCURED = True
                                    latest[-1][-1]="[red]Failed"
-                                   live.update(renderable=gen_table(latest))
+                                   live_display(live,latest)
                                    continue
                        # In case of no issue, we drop the status
                        # Status table only contain failure
                        latest=latest[:-1]
-                       live.update(renderable=gen_table(latest))
+                       live_display(live,latest)
                        
                    
         print(HTMLFOOTER,file=f)
@@ -363,10 +399,14 @@ with Live(gen_table([]), refresh_per_second=4) as live:
 # Refresh cursor
 oldprint('\033[?25h', end="")
 
-if ERROR_OCCURED:
-    sys.exit("Error occurred")
-else:
-    sys.exit(0)
+# When github action, error is tested by greping the summary html
+# like that the github action is not interrupted before uploading the
+# test report as an artifact.
+if not GHACTION:
+   if ERROR_OCCURED:
+       sys.exit("Error occurred")
+   else:
+       sys.exit(0)
 
 
 
